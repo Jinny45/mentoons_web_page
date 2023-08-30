15b46af05820d4a1556dc6983cbb4d87f2b413a8
jinny45 <jinashrigane45@gmail.com>
2023-08-31 02:13:50 +0530

    first commit

---
+++ script-final.js
@@ -0,0 +1,33 @@
+time_elm = document.getElementById("current-time")
+
+// var today = new Date();
+// var time = today.getDay() + ":" + today.getMonth() + ":" + today.getFullYear();
+
+// document.getElementById("current-time").innerText = time;
+
+var dt = new Date();
+document.getElementById("current-time").innerHTML = dt.toLocaleDateString();
+
+
+// ******************************************side pop up*************************************
+
+function sidebarcontent() {
+    var element1 = document.getElementById("hidden");
+
+    console.log(element1);
+    console.log("hello world");
+  
+    if (element1.style.display === "" || element1.style.display === "none") {
+      element1.style.display = "block";
+    } 
+  }
+
+  function close_sidebar() {
+    var element1 = document.getElementById("hidden");
+  
+      element1.style.display = "none";
+    
+  }
+
+
+    
